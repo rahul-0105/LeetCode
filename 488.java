aebb77cf2839c76895fa1dc9e01107a3be4a6a69
rahul-0105 <rahulak8216@gmail.com>
2025-12-01 22:42:03 +0530

    488

---
+++ 488.java
@@ -0,0 +1,23 @@
+
+    class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        
+
+        List<Integer> list = new ArrayList<> ();
+        HashSet <Integer> set = new HashSet<>();
+
+        for(int i :nums){
+            set.add(i);
+        }
+
+        for(int i=1;i<=nums.length;i++){
+            if(!(set.contains(i))){
+                list.add(i);
+            }
+        }
+        return list;
+
+
+    }
+}
+
